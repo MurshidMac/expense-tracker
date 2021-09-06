96bd889cfaf39a9a7707e2258b7c21be735c3be1
Ganidu Ashen <ganidu@calcey.com>
2021-09-06 15:19:30 +0530

    working code

---
+++ index.js
@@ -0,0 +1,58 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+// CSS
+import "./index.css";
+
+// import { data } from "./books";
+// import SpecificBook from "./Book";
+import { greeting } from "./testing/testing";
+import ExpenseItem from "./components/ExpenseItem";
+
+function BookList() {
+  console.log(greeting);
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+  return (
+    <React.Fragment>
+      {/* <section className="booklist">
+        {data.map((book, index) => {
+          return <SpecificBook key={book.id} {...book}></SpecificBook>;
+        })}
+      </section> */}
+      <div>
+        {expenses.map((elemn, index) => {
+          return (
+            <ExpenseItem
+              key={elemn.id}
+              title={elemn.title}
+              amount={elemn.amount}
+              date={elemn.date}
+            />
+          );
+        })}
+      </div>
+    </React.Fragment>
+  );
+}
+
+ReactDom.render(<BookList />, document.getElementById("root"));
